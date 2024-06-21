66759d60c22ffe1f149573ab95ecbe4d840add5d
Julian Humble <humblejulian@gmail.com>
2024-06-21 16:04:35 +0100

    Registration test 4 passed

--- rate-my-cup-react/tests/Registration.test.jsx
+++ rate-my-cup-react/tests/Registration.test.jsx
@@ -25,5 +25,32 @@ describe("Registration Tests", () => {
             // Assert
             expect(screen.getByText("Please enter a valid email address")).toBeInTheDocument();
         })
+
+        it("should render invalid password message after entering an invalid password", async () => {
+            // Arrange
+            // Act
+            await userEvent.type(passwordInput, "badpassword");
+            await userEvent.tab();
+            // Assert
+            expect(screen.getByText("Password must contain a special character, number and be at least 8 characters long")).toBeInTheDocument();
+        })
+
+        it("should not render invalid email message after entering valid email", async() => {
+            // Arrange
+            // Act
+            await userEvent.type(emailInput, "email@email.com")
+            await userEvent.tab();
+            // Assert
+            expect(screen.queryByText("Please enter a valid email address")).not.toBeInTheDocument();
+        })
+
+        it("should not render invalid email password after entering valid password", async () => {
+            // Arrange
+            // Act
+            await userEvent.type(passwordInput, "password1!");
+            await userEvent.tab();
+            // Assert
+            expect(screen.queryByText("Password must contain a special character, number and be at least 8 characters long")).not.toBeInTheDocument();
+        })
     })
 })
