f38fb883326f66f17a04891d464bbfa151e33739
Julian Humble <humblejulian@gmail.com>
2024-06-23 15:28:04 +0100

    addNewUser integration test 6 passed

--- user-service/test/integration/addNewUser.integration.test.js
+++ user-service/test/integration/addNewUser.integration.test.js
@@ -101,5 +101,14 @@ describe("addNewUser integration tests", () => {
             // Assert
             expect(response.status).to.equal(400);
         })
+
+        it("should respond with response code 400 if bad request - invalid password", async () => {
+            // Arrange
+            const invalidUser = { ...newUser, password: "password" }
+            // Act
+            const response = await request.post("/").send(invalidUser);
+            // Assert
+            expect(response.status).to.equal(400);
+        })
     })
 })
