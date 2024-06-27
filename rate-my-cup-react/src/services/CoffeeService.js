e9279833f4d1bc6133465c5bfbd05a8d22f55d7b
Julian Humble <humblejulian@gmail.com>
2024-06-27 11:06:08 +0100

    refactored axios request

--- rate-my-cup-react/src/services/CoffeeService.js
+++ rate-my-cup-react/src/services/CoffeeService.js
@@ -5,7 +5,7 @@ const API_URL = import.meta.env.VITE_APP_COFFEE_URL
 export default class CoffeeService {
     static locationSearch = async (postcode) => {
         try {
-            const response = await axios.get(`${API_URL}/location`, { params: { "postcode": postcode } })
+            const response = await axios.get(`${API_URL}/location?postcode=${postcode}`)
             return response.data
         } catch (error) {
             throw new Error(error.message)
