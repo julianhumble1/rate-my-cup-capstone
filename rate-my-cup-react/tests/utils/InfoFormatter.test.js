d24b8e2069eafcbd5ad34ca150eefb48e2d17ee8
Julian Humble <humblejulian@gmail.com>
2024-06-28 16:22:21 +0100

    format location results test 1 passed

--- rate-my-cup-react/tests/utils/InfoFormatter.test.js
+++ rate-my-cup-react/tests/utils/InfoFormatter.test.js
@@ -2,18 +2,7 @@ import InfoFormatter from "../../src/utils/InfoFormatter.js";
 
 describe("Info formatter tests", () => {
     describe("Format opening times", () => {
-        it("should return N/A if poi entered does not have an opening hours object", () => {
-            // Arrange
-            const poi = {};
-            // Act
-            const actual = InfoFormatter.formatOpeningTimes(poi)
-            // Assert
-            expect(actual).to.equal("N/A")
-        })
-
-        it("should return array of length 7 if poi entered is valid", () => {
-            // Arrange
-            const poi = {
+        const validPoi = {
                 "openingHours": {
                     timeRanges: [
                         {"startTime": {"date": "2024-06-28", "hour": 8, "minute": 30}, "endTime": {"date": "2024-06-28", "hour": 17, "minute": 30}},
@@ -26,10 +15,56 @@ describe("Info formatter tests", () => {
                     ]
                 }
             };
+        it("should return N/A if poi entered does not have an opening hours object", () => {
+            // Arrange
+            const invalidPoi = {};
             // Act
-            const actual = InfoFormatter.formatOpeningTimes(poi)
+            const actual = InfoFormatter.formatOpeningTimes(invalidPoi)
+            // Assert
+            expect(actual).to.equal("N/A")
+        })
+
+        it("should return array of length 7 if poi entered is valid", () => {
+            // Arrange
+            // Act
+            const actual = InfoFormatter.formatOpeningTimes(validPoi)
             // Assert
             expect(actual.length).to.equal(7)
+        })
+    })
+
+    describe("format location results tests", () => {
+
+        const testPoi = {
+        name: 'Test Location',
+        address: { freeformAddress: '123 Test St' },
+        phone: '123-456-7890',
+        url: 'https://testlocation.com'
+        };
+
+        beforeEach(() => {
+            vi.spyOn(InfoFormatter, "formatOpeningTimes").mockReturnValue([])
+        })
+
+        afterEach(() => {
+            vi.restoreAllMocks()
+        })
+
+
+        it("should format location results correctly with stubbed formatOpeningTimes if object is valid", () => {
+            // Arrange
+
+            // Act
+            const actual = InfoFormatter.formatLocationResults(testPoi)
+            // Assert
+            expect(actual).to.deep.equal({
+                name: "Test Location",
+                address: "123 Test St",
+                phone: "123-456-7890",
+                url: "https://testlocation.com",
+                openingHours: []
+            })
+
         })
     })
 })
