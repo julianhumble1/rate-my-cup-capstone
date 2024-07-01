313104231b29cdf477f01f6c3c28424916d0b28d
Julian Humble <humblejulian@gmail.com>
2024-07-01 21:12:59 +0100

    moved tomtom api key into dotenv

--- rate-my-cup-react/src/services/CoffeeService.js
+++ rate-my-cup-react/src/services/CoffeeService.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 const API_URL = import.meta.env.VITE_APP_COFFEE_URL
+const TOMTOM_API_KEY = import.meta.env.TOMTOM_API_KEY
 
 export default class CoffeeService {
     static locationSearch = async (postcode) => {
@@ -14,7 +15,7 @@ export default class CoffeeService {
 
     static getLocationDetails = async (id) => {
         try {
-            const response = await axios.get(`https://api.tomtom.com/search/2/place.json?entityId=${id}&key=sT17166UsrgptIJQcPeXqLcIb6EgnlNe&apiVersion=1&openingHours=nextSevenDays`)
+            const response = await axios.get(`https://api.tomtom.com/search/2/place.json?entityId=${id}&key=${TOMTOM_API_KEY}&apiVersion=1&openingHours=nextSevenDays`)
             return response.data.results[0]
         } catch (error) {
             throw new Error(error.message)
