e3ba0a93ba6a4f9f19489b9b7308832c25d0fe10
Julian Humble <humblejulian@gmail.com>
2024-07-01 18:02:06 +0100

    Drink Reviews Summary test 4 passed

--- rate-my-cup-react/tests/components/DrinkReviewsSummary.test.jsx
+++ rate-my-cup-react/tests/components/DrinkReviewsSummary.test.jsx
@@ -35,4 +35,43 @@ describe("Drink Reviews Summary tests", () => {
         // Assert
         expect(drinkName).toBeInTheDocument()
     })
+
+    it("should render no rates yet if reviews array is empty", async () => {
+        // Arrange
+        ReviewDataFormatter.arrangeReviewsByDrink.mockReturnValue({ "Latte": [] })
+        render(<MemoryRouter>
+            <DrinkReviewsSummary drinkType={drinkType}/>
+        </MemoryRouter>)
+        // Act
+        const noRatesYet = await screen.findByText("No Rates Yet!")
+        // Assert
+        expect(noRatesYet).toBeInTheDocument()
+    })
+
+    it("should render number of stars equal to value returned by average rating", async () => {
+        // Arrange
+        ReviewDataFormatter.arrangeReviewsByDrink.mockReturnValue({ "Latte": [{}] })
+        ReviewDataFormatter.calculateAverageRating.mockReturnValue(3)
+        render(<MemoryRouter>
+            <DrinkReviewsSummary drinkType={drinkType}/>
+        </MemoryRouter>)
+        // Act
+        const stars = await screen.findByText("★★★")
+        // Assert
+        expect(stars).toBeInTheDocument()
+    })
+
+    it("should render number of £ equal to value returned by mode price", async () => {
+        // Arrange
+        ReviewDataFormatter.arrangeReviewsByDrink.mockReturnValue({ "Latte": [{}] })
+        ReviewDataFormatter.calculateAverageRating.mockReturnValue(3)
+        ReviewDataFormatter.calculateModePrice.mockReturnValue(3)
+        render(<MemoryRouter>
+            <DrinkReviewsSummary drinkType={drinkType}/>
+        </MemoryRouter>)
+        // Act
+        const pounds = await screen.findByText("£££")
+        // Assert
+        expect(pounds).toBeInTheDocument()
+    })
 })
