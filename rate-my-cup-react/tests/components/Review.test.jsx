cc5a5a3f6418d43a689a3bb22c1e3420d13f6dea
Julian Humble <humblejulian@gmail.com>
2024-07-03 11:40:58 +0100

    review test 1 passed

---
+++ rate-my-cup-react/tests/components/Review.test.jsx
@@ -0,0 +1,68 @@
+import Review from "../../src/components/Review.jsx";
+import { render, screen, act } from "@testing-library/react"
+import InfoFormatter from "../../src/utils/InfoFormatter.js";
+import { MemoryRouter } from "react-router-dom";
+import ReviewService from "../../src/services/ReviewService.js";
+import CoffeeService from "../../src/services/CoffeeService.js";
+
+describe("review item tests", () => {
+
+    describe("initial review display tests", () => {
+
+        let review;
+
+        beforeEach(() => {
+            vi.mock("../../src/services/CoffeeService.js", () => ({
+                default: class {
+                    static getLocationDetails = vi.fn();
+                }
+            }))
+
+            vi.mock("../../src/utils/InfoFormatter.js", () => ({
+                default: class {
+                    static formatLocationResults = vi.fn();
+                }
+            }))
+
+            vi.mock("../../src/service/ReviewService.js", () => ({
+                default: class {
+                    static editReview = vi.fn();
+                }
+            }))
+
+            global.localStorage = {
+                getItem: vi.fn(),
+            }
+            
+            review = {
+                _id: "testID",
+                locationId: "testLocationId",
+                userId: "testReviewId",
+                drinkType: "Latte",
+                rating: 2,
+                price: 3,
+                comment: "test"
+            }
+
+        })
+        afterEach(() => {
+            vi.restoreAllMocks();
+        })
+
+        it("should not display edit button if local storage id doesn't match review userID or user is not admin", () => {
+            // Arrange
+            global.localStorage.getItem.mockImplementation((key) => {
+                if (key === "role") return "user";
+                if (key === "id") return "notTestId";
+                return null;
+            });
+            // Act
+            render(<MemoryRouter>
+                <Review review={review} />
+            </MemoryRouter>)
+            const edit = screen.queryByText("Edit")
+            // Assert
+            expect(edit).not.toBeInTheDocument();
+        })
+    })
+})
