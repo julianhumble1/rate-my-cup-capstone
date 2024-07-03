794a7948526f4846f8884b1fea7aa6cb19315267
Julian Humble <humblejulian@gmail.com>
2024-07-03 15:52:11 +0100

    added rating filter to home page

--- rate-my-cup-react/src/components/Home.jsx
+++ rate-my-cup-react/src/components/Home.jsx
@@ -13,6 +13,11 @@ const Home = () => {
 
   const [loading, setLoading] = useState(false)
 
+  const [priceFilter, setPriceFilter] = useState("Any")
+  const [ratingFilter, setRatingFilter] = useState("Any")
+
+  const drinkOptions = ["Latte", "Espresso", "Americano", "Cappuccino", "Mocha", "Flat White", "Tea", "Other"]
+
   const handleSearch = async () => {
     setLoading(true)
     try {
@@ -31,11 +36,20 @@ const Home = () => {
     <div className = "container text-center rounded-2 pb-3" id="search-box">
       <h1>Find your cup...</h1>
       <div className="row justify-content-center">
-        <div className="col-8 col-md-4 pb-md-4 pb-0">
+        <div className="col-8 col-md-4 pb-md-4 pb-2">
           <input type="text" id="postcode-box" className="form-control" placeholder="Postcode" onChange={(e) => setPostcodeSearch(e.target.value)}/>
         </div>
       </div>
-      <div className="row pt-3 pt-md-0">
+      <form>
+        <div className="row justify-content-center pb-2">
+          <div className="col-12 col-md-6 ">
+            <button type="button" className="btn btn-outline-dark col-10 col-md-12" id="search-button" data-testid="location-search-button" onClick={handleSearch}>Find My Cup!</button>
+            {postcodeError && <div className="text-danger">
+              {postcodeError}
+            </div>}
+          </div>
+        </div>
+        <div className="row pt-3 pt-md-0">
         <div className="d-none d-md-block col-md-4 ">
           <label htmlFor="coffee-choice">Drink Type</label>
         </div>
@@ -46,19 +60,18 @@ const Home = () => {
           <label htmlFor="min-price-choice">Price</label>
         </div>
       </div>
-      <form>
         <div className = "row mb-3">
         <div className = "col-12 col-md-4 mb-2 m-md-0">
           <select className="form-select" id="coffee-choice" defaultValue = "Any">
             <option value="Any">Any Drink Type</option>
-            <option value="Latte">Latte</option>
-            <option value="Espresso">Espresso</option>
-            <option value="Mocha">Mocha</option>
+            {drinkOptions.map((drinkOption, index) => (
+              <option key={index} value={drinkOption}>{drinkOption}</option>
+              ))}
           </select>
         </div>
         <div className="col-12 col-md-4 mb-2 m-md-0">
-          <select className="form-select" id="min-rating-choice" defaultValue = "Any">
-            <option value = "Any">Any Rating</option>
+          <select className="form-select" id="min-rating-choice" defaultValue = "Any" onChange={(e) => setRatingFilter(e.target.value)}>
+            <option value="Any">Any Rating</option>
             <option value="1">&#9733;</option>
             <option value="2">&#9733;&#9733;</option>
             <option value="3">&#9733;&#9733;&#9733;</option>
@@ -67,24 +80,17 @@ const Home = () => {
           </select>
         </div>
         <div className="col-12 col-md-4 mb-2 m-md-0">
-          <select className="form-select" id="price-rating-choice" defaultValue = "Any">
-            <option value = "Any">Any Price</option>
+          <select className="form-select" id="price-rating-choice" defaultValue = "Any" onChange={(e) => setPriceFilter(e.target.value)}>
+            <option value="Any">Any Price</option>
             <option value="1">£</option>
             <option value="2">£/££</option>
             <option value="3">£/££/£££</option>
           </select>
           </div>
         </div>
-        <div className="row justify-content-center">
-          <div className="col-12 col-md-6 ">
-            <button type="button" className="btn btn-outline-dark col-10 col-md-12" id="search-button" data-testid="location-search-button" onClick={handleSearch}>Find My Cup!</button>
-            {postcodeError && <div className="text-danger">
-              {postcodeError}
-            </div>}
-          </div>
-        </div>
+        
       </form>
-      <div className="row p-4 justify-content-center">
+      {/* <div className="row p-4 justify-content-center">
         <div className="col-md-6 col-12 text-center fs-3 ">
           Or search for a specific cafe...
         </div>
@@ -96,13 +102,13 @@ const Home = () => {
             <button className="btn d-block w-100" type="button" id="search-by-name-button">Find My Cup!</button>
           </div>
         </div>
-      </div>
+      </div> */}
       {loading &&
       <div>Loading...</div>
       }
       {locationResults &&
         locationResults.map((locationInfo, index) => (
-        <SearchResult locationInfo={locationInfo} key={index} />
+          <SearchResult locationInfo={locationInfo} key={index} priceFilter={priceFilter} ratingFilter={ratingFilter} />
         ))
       }
     </div>
--- rate-my-cup-react/src/components/SearchResult.jsx
+++ rate-my-cup-react/src/components/SearchResult.jsx
@@ -4,11 +4,14 @@ import { useEffect, useState } from "react"
 import ReviewService from "../services/ReviewService.js"
 import ReviewDataFormatter from "../utils/ReviewDataFormatter.js"
 
-const SearchResult = ({ locationInfo }) => {
+const SearchResult = ({ locationInfo, priceFilter, ratingFilter }) => {
 
     const locationId = locationInfo.id
 
     const [locationRating, setLocationRating] = useState()
+    const [locationPrice, setLocationPrice] = useState()
+    
+    const [matchesFilters, setMatchesFilters] = useState(true)
 
     useEffect(() => {
         const fetchReviewData = async () => {
@@ -16,6 +19,8 @@ const SearchResult = ({ locationInfo }) => {
                 const responseData = await ReviewService.getAllLocationReviews(locationId);
                 const rating = ReviewDataFormatter.calculateAverageRating(responseData)
                 setLocationRating(rating)
+                const price = ReviewDataFormatter.calculateModePrice(responseData)
+                setLocationPrice(price)
             } catch (e) {
                 console.log(e);
                 return null;
@@ -25,29 +30,46 @@ const SearchResult = ({ locationInfo }) => {
         fetchReviewData()
     }, [locationId])
 
+    useEffect(() => {
+        if (priceFilter === "Any" && ratingFilter === "Any") {
+            setMatchesFilters(true)
+            return
+        }
+        if (locationPrice > priceFilter || (locationRating < ratingFilter)) {
+            setMatchesFilters(false)
+        } else {
+            setMatchesFilters(true)
+        }
+    }, [priceFilter, ratingFilter])
+
     return (
-      <div className="container justify-content-center rounded-3 mt-3" id="result-box" data-testid="search-result">
-            <div className="row p-2 row-cols-2">
-                <div className="col">
-                    <div className="row ps-3 text-start fw-bold">
-                        <Link to={`/location/${locationInfo.id}`} className="location-name ps-0">
-                            {locationInfo.name}   
-                        </Link>
+    <>
+        {matchesFilters &&
+
+            <div className="container justify-content-center rounded-3 mt-3" id="result-box" data-testid="search-result">
+                    <div className="row p-2 row-cols-2">
+                        <div className="col">
+                            <div className="row ps-3 text-start fw-bold">
+                                <Link to={`/location/${locationInfo.id}`} className="location-name ps-0">
+                                    {locationInfo.name}   
+                                </Link>
+                            </div>
+                            <div className="row ps-3 text-start">
+                                {locationInfo.address}
+                            </div>
                     </div>
-                    <div className="row ps-3 text-start">
-                        {locationInfo.address}
-                    </div>
+                        <div className="text-end align-content-center fs-4">
+                            {locationRating > 0 && <div>
+                                {"★".repeat(locationRating)}
+                            </div>}
+                            {locationRating === 0 && <div>
+                                No Rates Yet
+                            </div>}
+                    </div>    
+                </div>
             </div>
-                <div className="text-end align-content-center fs-4">
-                    {locationRating > 0 && <div>
-                        {"★".repeat(locationRating)}
-                    </div>}
-                    {locationRating === 0 && <div>
-                        No Rates Yet
-                    </div>}
-            </div>    
-        </div>
-      </div>
+        }
+    </>
   )
 }
 
